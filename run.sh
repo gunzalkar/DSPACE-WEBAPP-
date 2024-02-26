9e86add820d2081e5fbcca2eb6ca907ad6fcfbcb
Kshitij Gunjalkar <42808290+gunzalkar@users.noreply.github.com>
2024-02-26 19:05:39 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,16 @@
+NGINX_CONF="/etc/nginx/sites-enabled/flask_app"
+echo "server {
+    listen 8020;
+
+    location / {
+            proxy_pass http://127.0.0.1:8000;
+            proxy_set_header Host \$host;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    }
+}" > $NGINX_CONF
+unlink /etc/nginx/sites-enabled/default
+systemctl restart nginx
+echo "Nginx and Gunicorn have been configured successfully."
+sleep 2
+ufw allow 8020
+gunicorn app:app –daemon
